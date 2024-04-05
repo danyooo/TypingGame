97f7b93017129438370f5b533653c5bcea2335c4
danyooo <danynorby0027@gmail.com>
2024-04-05 11:21:57 -0400

    Update dotnetsetup.bat Update Player.cs Player.cs now has the ability to read the player's inputs and select a name with it dotnetsetup now includes instructions on how to run the code

--- dotnetsetup.bat
+++ dotnetsetup.bat
@@ -9,3 +9,6 @@ SET PATH=D:\ICS4U\dotnetSDK;%PATH%
 @REM Temporary Solution--- Use CMD for the integrated terminal and run the above commands on it first, and it should work
 @REM find a way to automatically do this each new terminal instance for ease of access
 @REM set default profile in settings.json to ... "terminal.integrated.defaultProfile.windows": "Command Prompt",
+dotnet run
+
+@REM Just type in the terminal -- dotnetsetup.bat -- and it should run this fine
