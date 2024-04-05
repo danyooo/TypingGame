4392e48e600c628763e185c5bb0f35de62324b79
danyooo <danynorby0027@gmail.com>
2024-04-04 22:18:17 -0400

    Updates dotnetsetup.bat Temporary solution for VScode finding dotnet in order to run code found automating this process would be alot easier though

--- dotnetsetup.bat
+++ dotnetsetup.bat
@@ -6,4 +6,6 @@ SET DOTNET_MULTILEVEL_LOOKUP=0
 
 SET PATH=D:\ICS4U\dotnetSDK;%PATH%
 
-@REM THIS WORKS ^^ IN CMD ONLY? RECOGNISES SDK EXISTS
+@REM Temporary Solution--- Use CMD for the integrated terminal and run the above commands on it first, and it should work
+@REM find a way to automatically do this each new terminal instance for ease of access
+@REM set default profile in settings.json to ... "terminal.integrated.defaultProfile.windows": "Command Prompt",
