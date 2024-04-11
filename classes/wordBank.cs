92f25ad47ef90ec444760f2bb39b3889455a1459
danyooo <danynorby0027@gmail.com>
2024-04-10 21:25:43 -0400

    Updates wordBank class Adds method to choose a random word TBC: Display this chosen word in monogame(and also also make debug code that would actually have a list of nums to choose from)

--- classes/wordBank.cs
+++ classes/wordBank.cs
@@ -1,23 +1,27 @@
 
+// add System Usage to use "Random" in the code.
+using System;
 // WordBank class
-protected abstract class WordBank{
+ abstract class WordBank{
 // Empty word array that contains the set of words for an object
     // would be overrided by whatever object inherits it 
-string words[];
+string[] words;
 //add a string that would contain the chosen random word
 string chosenWord;
+//add a value to convert the random int chosen and set the value to the chosen word
+int randomVal;
 // add a instance of random to choose a random thing from the array list
 static Random rnd = new Random();
 //method to choose a random word from wordbank array
-public chooseWord(get{return words},chosenWord,listSize){
+public string chooseWord(){
     //set the list size to the array length
 int listSize = words.Length;
 // make ChosenWord = a random value in the list
-string chosenWord = random.Next(0,listSize);
+ randomVal = rnd.Next(0,listSize);
+ chosenWord = words[randomVal];
 // return the chosen word after this is called
-return{chosenWord};
+return(chosenWord);
 
 }
 // Method to display word
-public displayWord
 }
